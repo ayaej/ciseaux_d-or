94d3ed4ec16569623230438cc1fa84169c0fb886
ayajanati85 <ayajanati85@gmail.com>
2025-05-28 20:27:32 +0200

    build: output to docs + nojekyll

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// vite.config.js
 export default defineConfig({
   plugins: [react()],
-  base: '/ciseaux_d-or/',
+  base: './',
+  build: { outDir: 'docs', emptyOutDir: true }
 })
