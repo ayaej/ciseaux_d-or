4c4bebde76e5e6ffc7d02e384282c15a26bb95ee
Guendouz Hicham <hicham.guendouz@socianova.com>
2025-09-16 13:46:44 +0200

    Correction package.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,19 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-
-import Navbar from './components/Navbar';
-import Home from './pages/Home';             
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import Prestations from './pages/Prestations';
 import Contact from './pages/Contact';
-
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="min-h-screen bg-background text-light flex flex-col">
+    <Router>
       <Navbar />
-      
-      <main className="flex-grow">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/prestations" element={<Prestations />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </main>
-
-      <footer className="p-6 bg-anthracite text-gold text-center text-sm mt-16">
-        &copy; 2025 Ciseaux d’Or. Tous droits réservés.
-       <div className="mt-2 text-light/50 text-xs">
-         Conçu par <a href="https://www.linkedin.com/in/aya-el-janati-44b961240/" target="_blank" className="underline hover:text-gold">Aya El Janati</a>
-       </div>
-      </footer>
-
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/prestations" element={<Prestations />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 
